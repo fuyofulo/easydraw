2b42fe1d7416562f15d9973eb100c7df54cd0de2
fuyofulo <pheonixdiaz625@gmail.com>
2025-05-02 22:39:11 +0530

    progress

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -5,6 +5,20 @@ import { JWT_SECRET } from '@repo/backend-common/config';
 
 const wss = new WebSocketServer({ port: 4001 });
 
+function checkUser(token: string): string | null {
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if(typeof decoded == 'string') {
+        return null;
+    }
+
+    if(!decoded || !decoded.userId) {
+        return null;
+    }
+
+    return decoded.userId;
+}
+
 wss.on('connection', function connection(ws, request) {
     
     const url = request.url;
@@ -13,12 +27,10 @@ wss.on('connection', function connection(ws, request) {
     }
 
     const queryParams = new URLSearchParams(url.split('?')[1]);
-    const token = queryParams.get('token');
-    const decoded = jwt.verify(token!, JWT_SECRET!);
-
-    if(!decoded || !(decoded as JwtPayload).userId) {
+    const token = queryParams.get('token') || "";
+    const userAuthenticated = checkUser(token);
+    if(!userAuthenticated) {
         ws.close();
-        return;
     }
 
     ws.on('message', function message(data) {
